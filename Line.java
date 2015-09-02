084fc8ae93916631cf834e162f95eadf8fc6428a
victor <victortony@163.com>
2015-09-02 12:56:23 +0800

    delete method clone

--- Line.java
+++ Line.java
@@ -17,15 +17,6 @@ public class Line extends Element {
 		// changed = false;
 	}
 
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		// TODO Auto-generated method stub
-		Line line = new Line(getColor(), getLand());
-		line.setPointStart((PointF) pointStart.clone());
-		line.setPointEnd((PointF) pointEnd.clone());
-		return line;
-
-	}
 
 	@SuppressLint("ResourceAsColor")
 	@Override
